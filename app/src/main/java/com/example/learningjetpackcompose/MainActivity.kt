bc6cc3f3d6d990978a89186341c6bedf73dbe0ef
H2ODERP <110103899+H2ODERP@users.noreply.github.com>
2025-09-17 00:09:37 -0400

    Images and Icons

--- app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.learningjetpackcompose
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.InspectableModifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,29 +50,64 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        color = Color.Blue,
-        fontSize = 30.sp,
-        modifier = modifier
-            .background(Color.Green)
-            .padding(16.dp)
-            //this changes the background of the padding, which was
-            //created after the background green was applied
-            .background(Color.Red)
-    )
+//    Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
+//        contentDescription = null,
+//        modifier = Modifier.background(Color.Black)
+//    )
+//    if(name.length > 5){
+//        Icon(imageVector = Icons.Default.Add,
+//            contentDescription = null
+//        )
+//    }
+    Column{
+        for(i in 1..10){
+            Icon(imageVector = Icons.Default.Face,
+                contentDescription = null
+            )
+        }
+    }
 
-    Text(
-        text = "other text",
-        color = Color.Blue,
-        fontSize = 30.sp
-    )
+
+
+
+//    Box(
+//        modifier = Modifier
+//            //.fillMaxWidth or fillMaxHeight
+//            //.fillMaxSize()
+//            .size(400.dp)
+//    ){
+//        Text(
+//            text = "Hello $name!",
+//            color = Color.Blue,
+//            fontSize = 30.sp,
+//            modifier = Modifier.align(Alignment.BottomEnd)
+//        )
+//
+//        Text(
+//            text = "other text",
+//            color = Color.Blue,
+//            fontSize = 30.sp
+//        )
+//    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearningJetpackComposeTheme {
         Greeting("Android")
+    }
+}
+
+// @Preview(showBackground = true)
+@Composable
+fun ProfileCard() {
+    Row {
+        Column {
+            Text("Ryan")
+            Text("Locked In")
+        }
     }
 }
